56db61ee2070892184165be0ac55a6d6044f5cac
micke95 <milan.milicevic@htecgroup.com>
2020-12-27 17:57:13 +0100

    chore: remove setup files and add some test case

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useMnM } from '@mnm-tech/provider';
+import { random } from 'lodash';
 
 function App() {
-  return <div>321</div>;
+  const { setItemList } = useMnM();
+
+  const [randomNumber, setRandomNumber] = useState(-1);
+
+  const setItemListIntervalHandler = () => {
+    setInterval(() => {
+      const generatedNumber = random(1, 10);
+      setRandomNumber(generatedNumber);
+      setItemList([generatedNumber]);
+    }, 5000);
+    return null;
+  };
+
+  useEffect(() => {
+    setItemListIntervalHandler();
+  }, []);
+
+  return <div>{`Generated item is: ${randomNumber}`}</div>;
 }
 
 export default App;
