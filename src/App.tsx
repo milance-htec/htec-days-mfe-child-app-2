9d603f6d0aed5f0a71fa20a7eeb61a1aaf56c9a0
micke95 <milan.milicevic@htecgroup.com>
2020-12-28 01:19:57 +0100

    feat: add item list

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { useMnM } from '@mnm-tech/provider';
 import { LinearProgress } from '@material-ui/core';
-import { random } from 'lodash';
 
 import 'App.scss';
 
 //@ts-ignore
-const Input = React.lazy(() => import('mnmMfeChildApp1/components/Input'));
+const Button = React.lazy(() => import('mnmMfeChildApp1/components/Button'));
 
 function App() {
-  const { itemList = [] } = useMnM();
+  const { itemList, setItemList } = useMnM();
+
+  const onItemDelete = (index: number) => {
+    const newList = itemList.filter((item, itemIndex) => itemIndex !== index);
+    setItemList(newList);
+  };
 
   return (
     <>
       <span className="mfe-child-app-2__heading">Child App #2</span>
 
-      <React.Suspense fallback={<LinearProgress />}>
-        <Input />
-      </React.Suspense>
-
       <div className="mfe-child-app-2">
         {itemList.map((item: string, index: number) => (
-          <div key={index}>{item}</div>
+          <div className="mfe-child-app-2__list-item" key={index}>
+            <div className="mfe-child-app-2__list-item--name">{item}</div>
+            <Button
+              onClick={() => {
+                onItemDelete(index);
+              }}
+            >
+              Delete
+            </Button>
+          </div>
         ))}
       </div>
     </>
